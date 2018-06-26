1227350174f02ae0f9d5c87b97130c67a8e201e2
Jacob <nyseth@localhost.localdomain>
2018-06-26 15:16:43 -0400

    Dashboard

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -5,16 +5,20 @@ import { Route } from '@angular/router';
 import { LessonShowPageComponent } from '../pages/lessons/lesson-show-page.component';
 import { CourseShowPageComponent } from '../pages/courses/course-show-page.component';
 import { LoginShowPageComponent } from '../pages/login/login-show-page.component';
+import { DashboardShowPageComponent } from '../pages/dashboard/dashboard-show-page.component';
+
+
+
 export const routes: Route[] = [
 	// { path: '/', component: LessonShowPageComponent }
 	{ path: 'courses/lesson', component: LessonShowPageComponent },
   // { path: 'lesson', component: DashboardPageComponent },
 	// { path: 'courses/lessons/create', component: LessonCreatePageComponent},
   // { path: 'create-lesson', component: LessonCreatePageComponent },
-  // { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'dashboard', component: DashboardShowPageComponent },
   // { path: 'courses', component: CoursesShowPageComponent },
- { path: 'course', component: CourseShowPageComponent },
+  { path: 'course', component: CourseShowPageComponent },
   // { path: 'create-course', component: CourseCreatePageComponent },
- { path: 'login', component: LoginShowPageComponent },
+  { path: 'login', component: LoginShowPageComponent },
   // { path: 'signup', component: SignupPageComponent },
 ];
