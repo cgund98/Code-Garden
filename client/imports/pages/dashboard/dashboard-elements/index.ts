8653b8e4e146b40391a655f87982a6b8f741074e
nekai45 <sharpjacob657@gmail.com>
2018-06-26 15:19:01 -0400

    i think i fixed my mistakke

---
+++ client/imports/pages/dashboard/dashboard-elements/index.ts
@@ -0,0 +1,1 @@
+export const DASHBOARD_ELEMENTS_DECLARATIONS = []
---
+++ client/imports/pages/dashboard/dashboard-show-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import template from './dashboard-show-page.component.html';
+
+@Component({
+  selector: 'dashboard-show-page',
+  template
+})
+
+export class DashboardShowPageComponent {}
