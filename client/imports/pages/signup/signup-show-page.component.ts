01f6a3002dfb0659066cf105a711f0253b62ee3f
Callum Gundlach <gundlachcallum@gmail.com>
2018-06-29 22:28:36 -0400

    Fixed password matching

--- client/imports/pages/signup/signup-show-page.component.ts
+++ client/imports/pages/signup/signup-show-page.component.ts
@@ -19,105 +19,36 @@ export class SignupShowPageComponent implements OnInit {
 
   constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {}
 
-
-
-  validateEqual(): ValidatorFn {
-      console.log("test");
-      return (confirm: FormControl): { [key: string]: any } | null => {
-      console.log("confirm", confirm);
-      console.log("confirm-parent", confirm.parent);
-      console.log("password", this.signupForm.get('password'));
-    //   if (confirm){
-    //   console.log(this.signupForm.value.password);
-    // }
-      // const  = confirm.value;
-      const password = this.signupForm.get('password').value;
-      console.log("pass", password);
-      console.log("value", confirm.value);
-      if (password && confirm !== confirm.value) {
-        console.log("check-tew");
-        return {'mismatch': true};
-      }
-      console.log("nul-tew");
-      return null;
-    };
-}
     ngOnInit() {
       this.signupForm = this.formBuilder.group({
         email: ['', Validators.required],
         name: ['', Validators.required],
         username: ['', Validators.required],
         password: ['', [Validators.required, Validators.minLength(6)]],
-        confirm: ['', [Validators.required, this.validateEqual.bind(this)]]
+        confirm: ['', [Validators.required,]]
       });
-      console.log(this.validateEqual);
-      // this.signupForm = new FormGroup ({
-      // email: new FormControl('', [Validators.required]),
-      // name: new FormControl('', [Validators.required]),
-      // username: new FormControl('', [Validators.required]),
-      // password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      // confirm: new FormControl('', [Validators.required, this.validateEqual(this.signupForm.controls['password'].value)])
-      // // }. { validator: this.validateEqual
-      // });
 
       this.error='';
 
-}
+    }
 
+    checkValids() {
+        if (this.signupForm.valid && this.passMatch()) {
+            return true;
+        }
+        return false;
+    }
 
+    passMatch() {
+        const password = this.signupForm.get('password').value;
+        const confirm = this.signupForm.get('confirm').value;
+        return password == confirm;
+    }
 
-    //
-    //   this.signupForm = this.formBuilder.group({
-    //     "password":this.signupForm,
-    //     "confirm":this.signupForm.confirm
-    //   }, {
-    //     validator: this.validatePasswordConfirmation
-    //   });
-    // }
-  //
-  // validatePasswordConfirmation(group: FormGroup) {
-  //   return (control: AbstractControl): {[key: string]: any} | null{
-  //     const confirm = group.value.password;
-  //     if (confirm && control !== control.value) {
-  //       return { 'mismatch': true};
-  //     }
-  //     return null;
-  //   };
-  // }
-
-
-//   ngOnInit() {
-//
-//     this.signupForm = new FormGroup ({
-//       email: new FormControl('', [Validators.required]),
-//       name: new FormControl('', [Validators.required]),
-//       username: new FormControl('', [Validators.required]),
-//       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-//       confirm: new FormControl('', [Validators.required, validateEqual()])
-//     })
-//     this.error='';
-//
-//
-
-
-
-
-
-// validateEqual() {
-//    return this.signupForm.get('password') === this.signupForm.get('confirm')
-//       ? null : {'mismatch': true};
-// }
-    // checkPasswords(this.signupForm) { // here we have the 'passwords' group
-    // let pass = this.signupForm.value.password;
-    // let confirmPass = this.signupForm.value.confirm;
-    //
-    // return pass === confirmPass ? null : { notSame: true }
-    // this.error = '';
-  // }
   signup() {
 
       console.log('Creating...');
-    if (this.signupForm.valid) {
+    if (this.checkValids()) {
         Accounts.createUser({
           email: this.signupForm.value.email,
           name: this.signupForm.value.name,
@@ -132,82 +63,6 @@ export class SignupShowPageComponent implements OnInit {
             this.router.navigate(['/login']);
           }
         });
-      // Accounts.createUser({
-      //   email: this.signupForm.value.email,
-      //   name: this.signupForm.value.name,
-      //   username: this.signupForm.value.username,
-      //   password: this.signupForm.value.password
-      // }, (err) => {
-      //   if (err) {
-      //     this.zone.run(() => {
-      //       this.error = err;
-      //     });
-      //   } else {
-      //     this.router.navigate(['/']);
-      //   }
-      // });
     }
   }
 }
-
-
-
-
-//   ngOnInit() {
-//     this.newUserForm = new FormGroup ({
-//       email: new FormControl('', Validators.required),
-//       name: new FormControl('', Validators.required),
-//       username: new FormControl('', [Validators.required, Validators.minLength(5)]),
-//       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-//       passwordConfirm: new FormControl('', Validators.required),
-//     })
-//   }
-//   get email() { return this.newUserForm.get('email'); }
-//   get username() { return this.newUserForm.get('username'); }
-//   get password() { return this.newUserForm.get('password'); }
-//   get passwordConfirm() { return this.newUserForm.get('passwordConfirm'); }
-//   get name() { return this.newUserForm.get('name'); }
-//
-//   submit() {
-//
-//    // 'submit #signup': function(event, template) {
-//     var user;
-//     // Collect Data and Validate
-//
-//     user = {
-//       username: this.username,
-//       password: this.password,
-//       email: this.email,
-//       profile: {
-//         name: this.name,
-//         // etc
-//       }
-//     }
-//
-//     // Accounts.validateNewUser(function (user) {
-//     //   // Ensure user name is long enough
-//     //   if (this.username.length < 5) {
-//     //     throw new Meteor.Error(403, 'Your username needs at least 5 characters');
-//     //   }
-//     //
-//     //   var passwordTest = new RegExp("(?=.{6,}).*", "g");
-//     //   if ((this.password == this.passwordConfirm) == false){
-//     //     throw new Meteor.Error(403, 'Your Password\'s must match');
-//     //   }
-//     //   if (passwordTest.test(this.password) == false) {
-//     //     throw new Meteor.Error(403, 'Your password is too weak!');
-//     //   }
-//     //
-//     //   return true;
-//     // });
-//
-//     // Post user to server for creation
-//     Accounts.createUser(user, function (error) {
-//     if (error) {
-//       // :(
-//       console.log(error);
-//     }
-//   });
-//
-// }
-// }
